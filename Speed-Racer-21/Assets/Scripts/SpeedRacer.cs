62505cbb05b0ff22f0b6a3f73b0892183c7a7b2a
aliceaavxoo <aliceaav@tlu.ee>
2023-11-10 21:35:14 +0200

    MyTry
    Followed the instructions

--- Speed-Racer-21/Assets/Scripts/SpeedRacer.cs
+++ Speed-Racer-21/Assets/Scripts/SpeedRacer.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
+    string CarModel = "GTR R35";
+    string engineType = "V6, Twin Turbo";
+    
+    int carWeight = 1609;
+    int yearMade = 2009;
+    
+    float maxAcceleration = 0.98f;
+    
+    bool isCarTypeSedan = false;
+    bool hasFrontEngine = true;
 
+void Start()
+{
+    print("This car is a " + CarModel + " and it is quite powerful, look at the engine " + engineType + ".");
 
-    void Start()
+    CheckWeight();
+
+    if (yearMade <= 2009)
     {
+        print("The car was introduced in " + yearMade + ".");
+        int carAge = CalculateAge (yearMade);
 
+        print("That makes the car " + carAge + " years old.");
     }
-
-    void Update()
+    else
     {
-
+        print("The car was introduced in 2010's.");
+        print("It's maximum acceleration is " + maxAcceleration + " m/s2.");
     }
+   
+    print(CheckCharacteristics());
+
+}
+ 
+void CheckWeight()
+ {
+    if (carWeight < 1500)
+    {
+    print("The " + CarModel + " weighs less than 1500 kg.");
+    }
+    else
+    {
+    print ("The " + CarModel + " weighs more than 1500 kg.");
+    }
+}
+
+int CalculateAge(int yearMade)
+{
+return 2023 - yearMade;
+}
+
+
+string CheckCharacteristics()
+{
+    if (isCarTypeSedan)
+    {
+    return "The car is a sedan.";
+    }
+    else if (hasFrontEngine)
+    {
+    return "The racer is not a sedan, but has a front engine.";
+    }
+    else
+    {
+    return "It's not a sedan, nor does it have a front engine.";
+    }
+}
 }
